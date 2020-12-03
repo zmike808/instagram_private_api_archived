46b49f5cf34337f220112a82f83be0988bf07db6
Michael Zemsky <zmike808@gmail.com>
2020-12-03 12:27:41 +0000

    magic code grab

--- examples/login_and_solving_challenge.py
+++ examples/login_and_solving_challenge.py
@@ -1,6 +1,9 @@
 import json
 import codecs
 import re
+import email
+import imaplib
+
 
 from instagram_private_api import Client, ClientError, ClientCheckpointRequiredError
 
@@ -25,9 +28,45 @@ def onlogin_callback(api, new_settings_file):
         print('SAVED: {0!s}'.format(new_settings_file))
 
 
-username = 'username'
-password = 'password'
+username = ''
+password = ''
 settings_file = '/path/to/file/settings_{}.json'.format(username)
+
+CHALLENGE_EMAIL = ''
+CHALLENGE_PASSWORD = ''
+
+def get_code_from_email(username):
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(CHALLENGE_EMAIL, CHALLENGE_PASSWORD)
+    mail.select("inbox")
+    result, data = mail.search(None, "(UNSEEN)")
+    assert result == "OK", "Error1 during get_code_from_email: %s" % result
+    ids = data.pop().split()
+    for num in reversed(ids):
+        mail.store(num, "+FLAGS", "\\Seen")  # mark as read
+        result, data = mail.fetch(num, "(RFC822)")
+        assert result == "OK", "Error2 during get_code_from_email: %s" % result
+        msg = email.message_from_string(data[0][1].decode())
+        payloads = msg.get_payload()
+        if not isinstance(payloads, list):
+            payloads = [msg]
+        code = None
+        for payload in payloads:
+            body = payload.get_payload(decode=True).decode()
+            if "<div" not in body:
+                continue
+            match = re.search(">([^>]*?({u})[^<]*?)<".format(u=username), body)
+            if not match:
+                continue
+            print("Match from email:", match.group(1))
+            match = re.search(r">(\d{6})<", body)
+            if not match:
+                print('Skip this email, "code" not found')
+                continue
+            code = match.group(1)
+        return code
+    return False
+
 
 api = Client(username, password, on_login=lambda x: onlogin_callback(x, settings_file))
 try:
@@ -44,6 +83,10 @@ except ClientCheckpointRequiredError as e:
     account_id = match_dict['account_id']
     identifier = match_dict['identifier']
 
-    res = api.choose_confirm_method(account_id, identifier)  # confirm_method param has default value 1, you can pass 0
-    code = input('Enter code from email: ')
+    res = api.choose_confirm_method(account_id, '1')  # confirm_method param has default value 1, you can pass 0
+    magic_code = get_code_from_email(username)
+    if magic_code:
+        code = magic_code
+    else:
+        code = input('Enter code from email: ')
     api.send_challenge(account_id, identifier, code)
